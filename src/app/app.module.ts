d87f108410aec2613d865e081dcdeb2bd2871013
Victor Murillo <vimurillo@gmail.com>
2018-01-25 02:07:51 +0000

    Navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { ExchangeComponent } from './components/exchange/exchange.component';
 
 import { ExchangeService } from './services/exchange.service';
 import { HttpClientModule } from '@angular/common/http';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExchangeComponent
+    ExchangeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
